3628ae787ae2ead722a2c91ee3fab198f2df236e
yulucat <103909960+yulucat@users.noreply.github.com>
2021-12-30 15:31:30 +0000

    fix indent

--- lib/bentov.ml
+++ lib/bentov.ml
@@ -8,3 +8,12 @@ type histogram = {
   max_bins : int;
   num_bins : int;
   bins : bin list;
+  range : (float * float) option; (* (min * max) *)
+  total_count : int;
+}
+
+let max_bins h =
+  h.max_bins
+
+let num_bins h =
+  h.num_bins
