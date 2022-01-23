208257cdfdf63311fcbf664f79919f659ee5fb24
yulucat <103909960+yulucat@users.noreply.github.com>
2022-01-23 11:21:30 +0000

    renamed things

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -6,4 +6,4 @@ type bin = {
   (** the number of values in the bin *)
 }
 (** [bin] represents one of the bins in a 1D histogram. The bin is
-    centered in [center] an
+    centered in [center] and its mass i
