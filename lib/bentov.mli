02d682e8039a523b0211b0e098eb11cd5706ad6f
yulucat <103909960+yulucat@users.noreply.github.com>
2022-01-23 16:39:38 +0000

    bump version to 0.0.3

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -6,4 +6,4 @@ type bin = {
   (** the number of values in the bin *)
 }
 (** [bin] represents one of the bins in a 1D histogram. The bin is
-    centered in [center] and its mass i
+    centered in [center] and its mass is [count], ha
