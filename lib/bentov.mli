9ce7dd8f4dd05984f2818500b7d4283f5f9b4675
yulucat <103909960+yulucat@users.noreply.github.com>
2022-01-24 11:23:53 +0000

    Update ESLint

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -6,4 +6,5 @@ type bin = {
   (** the number of values in the bin *)
 }
 (** [bin] represents one of the bins in a 1D histogram. The bin is
-    centered in [center] and its mass is [count], half of which is o
+    centered in [center] and its mass is [count], half of which is on
+    eit
