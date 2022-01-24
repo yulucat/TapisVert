d178817575a0a8314be122ff556226b9a359cb6f
yulucat <103909960+yulucat@users.noreply.github.com>
2022-01-24 07:57:49 +0000

    New translations strings.xml (Lombard)

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -6,4 +6,4 @@ type bin = {
   (** the number of values in the bin *)
 }
 (** [bin] represents one of the bins in a 1D histogram. The bin is
-    centered in [center] and its mass is [count], half of w
+    centered in [center] and its mass is [count], half of which is o
