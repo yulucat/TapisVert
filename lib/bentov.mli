4a803c498f9099bf27adb9b842033bc0d5432d7c
yulucat <103909960+yulucat@users.noreply.github.com>
2022-02-23 07:49:41 +0000

    More UI fixes

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -20,4 +20,4 @@ val num_bins : histogram -> int
     number of bins; equivalent to [List.length (bins h)] *)
 
 val max_bins : histogram -> int
-(** [max_bins h] returns the maximum number of bins of
+(** [max_bins h] returns the maximum number of bins of this histogr
