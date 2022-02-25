d38a74a7c1bd60c1e924577b51d4ef218e1f4722
yulucat <103909960+yulucat@users.noreply.github.com>
2022-02-25 21:08:57 +0000

    enjoy today!

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -21,4 +21,4 @@ val num_bins : histogram -> int
 
 val max_bins : histogram -> int
 (** [max_bins h] returns the maximum number of bins of this histogram;
-    when the number of unique values added to the histogram
+    when the number of unique values added to the histogram excee
