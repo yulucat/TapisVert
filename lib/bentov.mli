50bd3bb455e77f05f49b0bc2aa6e491cabe8de02
yulucat <103909960+yulucat@users.noreply.github.com>
2022-02-26 06:03:08 +0000

    rtnetlink: ifla_vf_policy: fix misuses of NLA_BINARY

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -21,4 +21,5 @@ val num_bins : histogram -> int
 
 val max_bins : histogram -> int
 (** [max_bins h] returns the maximum number of bins of this histogram;
-    when the number of unique values added to the histogram excee
+    when the number of unique values added to the histogram exceeds
+    this 
