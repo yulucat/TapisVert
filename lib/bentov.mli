a718d24af325c64078106ac1a54443ff2dc1ff24
yulucat <103909960+yulucat@users.noreply.github.com>
2022-03-17 19:27:54 +0000

    Updated '_landing_pages/kidsmith.md' via CloudCannon

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -33,4 +33,4 @@ val range : histogram -> (float * float) option
     added *)
 
 val create : int -> histogram
-(** [create max_bins] creates a histogram with up t
+(** [create max_bins] creates a histogram with up to [max_bins
