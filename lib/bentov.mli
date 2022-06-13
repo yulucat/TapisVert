76cf7b3dfcbbefadf5b99ff4be2c1ca41cd405dc
yulucat <103909960+yulucat@users.noreply.github.com>
2022-06-13 21:41:00 +0000

    Add myself.

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -41,4 +41,4 @@ val add : float -> histogram -> histogram
 
 val addc : float -> int -> histogram -> histogram
 (** [addc v c h] adds a value to [v] to histogram [h] with count [c],
-    returning the updated h
+    returning the updated histogram.  It
