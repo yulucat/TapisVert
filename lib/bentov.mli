16e7df5ffa4c2f5d2f77c9d942e8cb6ae681310a
yulucat <103909960+yulucat@users.noreply.github.com>
2022-06-17 08:45:23 +0000

    SCSI: Fix incorrect memset in bnx2fc_parse_fcp_rsp

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -41,4 +41,4 @@ val add : float -> histogram -> histogram
 
 val addc : float -> int -> histogram -> histogram
 (** [addc v c h] adds a value to [v] to histogram [h] with count [c],
-    returning the updated histogram.  It
+    returning the updated histogram.  It is equivalen
