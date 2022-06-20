9dba1e6781af0cc19bf5a3c812fc37580b3efc35
yulucat <103909960+yulucat@users.noreply.github.com>
2022-06-20 11:11:30 +0000

    Translated using Weblate (Hungarian)

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -41,4 +41,4 @@ val add : float -> histogram -> histogram
 
 val addc : float -> int -> histogram -> histogram
 (** [addc v c h] adds a value to [v] to histogram [h] with count [c],
-    returning the updated histogram.  It is equivalent to 
+    returning the updated histogram.  It is equivalent to calling [add
