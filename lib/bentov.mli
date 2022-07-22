cc1a1bf087eff4c22026d6a293734ba6771b11b9
yulucat <103909960+yulucat@users.noreply.github.com>
2022-07-22 21:15:38 +0000

    another cleanup

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -46,4 +46,4 @@ val addc : float -> int -> histogram -> histogram
 
 val merge : histogram list -> int -> histogram
 (** [merge h_list max_bins] creates a new histogram from the histograms
-    in [h_list], whose size is no bigger than [max_bins
+    in [h_list], whose size is no bigger than [max_bins] *)
