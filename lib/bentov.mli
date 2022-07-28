a328c26a0614a3a1897510794239b06e8983b898
yulucat <103909960+yulucat@users.noreply.github.com>
2022-07-28 06:16:20 +0000

    modify readme

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -50,4 +50,4 @@ val merge : histogram list -> int -> histogram
 
 val sum : histogram -> float -> float
 (** [sum hist b] returns an estimate of the number of points in the
-    interval [[−infinity,b]
+    interval [[−infinity,b]]. For an
