5735bb74d37631a3a47aa3a63013cd5314eab88c
yulucat <103909960+yulucat@users.noreply.github.com>
2022-09-08 15:51:01 +0000

    some code clean-up

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -69,4 +69,4 @@ val uniform : histogram -> int -> (int * float) list
     [Empty] if the histogram has not been [add]'d any data.*)
 
 val mean : histogram -> float
-(** [mean hist] returns an estimate of the mean of the distri
+(** [mean hist] returns an estimate of the mean of the distribution
