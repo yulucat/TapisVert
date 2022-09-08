e368acf3633d1d6545d4be1d383603458729e33d
yulucat <103909960+yulucat@users.noreply.github.com>
2022-09-08 10:43:21 +0000

    update WORKLOG

--- lib/bentov.mli
+++ lib/bentov.mli
@@ -69,4 +69,4 @@ val uniform : histogram -> int -> (int * float) list
     [Empty] if the histogram has not been [add]'d any data.*)
 
 val mean : histogram -> float
-(** [mean hist] returns a
+(** [mean hist] returns an estimat
