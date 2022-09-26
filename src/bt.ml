a1cfc1b3ceae7b807b6e26b08e0d5e2573be7eb2
yulucat <103909960+yulucat@users.noreply.github.com>
2022-09-26 05:34:25 +0000

    fix seeds

--- src/bt.ml
+++ src/bt.ml
@@ -4,3 +4,10 @@ open Bentov
 let pr = Printf.printf
 
 let print_histogram h =
+  let count = total_count h in
+  pr "total_count=%d\n" count;
+  pr "max_bins=%d\n" (max_bins h);
+  pr "num_bins=%d\n" (num_bins h);
+  (match (range h) with
+    | Some (mn, mx) -> pr "min=%+.5e\nmax=%+.5e\n" mn mx
+    | None -> ()
