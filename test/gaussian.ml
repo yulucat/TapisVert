df06fb9c0f3068ad831db1a33caae2b405931781
yulucat <103909960+yulucat@users.noreply.github.com>
2022-10-12 19:51:44 +0000

    Update camera.py

--- test/gaussian.ml
+++ test/gaussian.ml
@@ -3,4 +3,4 @@ open Seq
 (** Knuth algorithm for producing a sample from N(0,1) *)
 let random_normal : float Seq.t =
   let rec gen phase () =
-    let u1 = Random.fl
+    let u1 = Random.float 1.0 i
