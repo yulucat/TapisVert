a5271414dd045d3883a10e6da9596f7d1651b636
yulucat <103909960+yulucat@users.noreply.github.com>
2022-10-12 23:09:21 +0000

    error fix

--- test/gaussian.ml
+++ test/gaussian.ml
@@ -3,4 +3,5 @@ open Seq
 (** Knuth algorithm for producing a sample from N(0,1) *)
 let random_normal : float Seq.t =
   let rec gen phase () =
-    let u1 = Random.float 1.0 i
+    let u1 = Random.float 1.0 in
+    let u2 =
