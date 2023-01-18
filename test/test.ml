0af35a6e1be3ac84526bcdfd4ea519dcb47963d6
yulucat <103909960+yulucat@users.noreply.github.com>
2023-01-18 22:03:37 +0000

    update javadoc

---
+++ test/test.ml
@@ -0,0 +1,4 @@
+
+(* In this test, we measure the error between the true quantiles of
+   data, and the quantiles as computed through the [Bentov] histogram
+   approximation module. The data is drawn from a mixture of two
