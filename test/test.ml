7edd6d5f7405b03ebdff598e8cd6a2565fc15846
yulucat <103909960+yulucat@users.noreply.github.com>
2023-02-03 16:19:06 +0000

    One more time

--- test/test.ml
+++ test/test.ml
@@ -2,3 +2,11 @@
 (* In this test, we measure the error between the true quantiles of
    data, and the quantiles as computed through the [Bentov] histogram
    approximation module. The data is drawn from a mixture of two
+   Gaussians, N(2,1) and N(5,1), where the mixing coefficient is 1/2.
+   We compute the approximate quantiles in two ways: In the frist, we
+   simply add each sample into a [Bentov.histogram]. We call this
+   histogram [mixed]. In the second, we add a datum to one of two
+   [Bentov.histogram]'s, one associated with each of the Guassians. We
+   then merge these two histograms using [Bentov.merge]. We call the
+   result of merging the two sub-histograms [merged]. Finally, we
+   compute and print the mean-square-error between the true quantiles
